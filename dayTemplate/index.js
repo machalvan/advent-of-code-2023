a106ca39c8d9c32e79925c36baa2214bf4fe8e64
Marcus Hansson <machalvan@hotmail.com>
2023-11-30 15:08:21 +0100

    First commit

---
+++ dayTemplate/index.js
@@ -0,0 +1,13 @@
+require('../utils')()
+
+const parse = input => input
+
+const part1 = input => {
+  return parse(input)
+}
+
+const part2 = input => {
+  return
+}
+
+module.exports = { part1, part2 }
