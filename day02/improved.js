670911a4926f174245fa265e7c87ceea97a68fff
Marcus Hansson <machalvan@hotmail.com>
2023-12-02 08:30:49 +0100

    Add improved day 2

---
+++ day02/improved.js
@@ -0,0 +1,33 @@
+require('../utils')()
+
+const part1 = input => {
+  return input.toLines().reduce(
+    (sum, line, i) =>
+      sum +
+      (line
+        .match(/(\d+ \w+)/g)
+        .map(cubes => cubes.split(' '))
+        .every(
+          ([num, color]) => num <= { red: 12, green: 13, blue: 14 }[color]
+        ) && i + 1),
+    0
+  )
+}
+
+const part2 = input => {
+  return input.toLines().reduce(
+    (sum, line) =>
+      sum +
+      ['red', 'green', 'blue']
+        .map(color =>
+          line
+            .match(new RegExp(`(\\d+) ${color}`, 'g'))
+            .map(cubes => cubes.split(' ')[0])
+            .max()
+        )
+        .prod(),
+    0
+  )
+}
+
+module.exports = { part1, part2 }
