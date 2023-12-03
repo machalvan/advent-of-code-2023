ac2844ce2a506b3d3139c6d3d69f1c92143f1035
Marcus Hansson <machalvan@hotmail.com>
2023-12-03 23:22:29 +0100

    Add improved day 3

---
+++ day03/improved.js
@@ -0,0 +1,80 @@
+require('../utils')()
+
+const part1 = input => {
+  let grid = input.toGrid()
+
+  let res = 0
+  for (let i = 0; i < grid.length; i++) {
+    let row = grid[i]
+
+    let num = ''
+    let isPart = false
+    for (let j = 0; j < row.length; j++) {
+      let cell = row[j]
+      if (cell.isNum()) {
+        num += cell
+
+        grid.forEachSurrounding(i, j, char => {
+          if (char !== undefined && isNaN(char) && char !== '.') {
+            isPart = true
+          }
+        })
+      } else {
+        res += isPart ? +num : 0
+        num = ''
+        isPart = false
+      }
+    }
+
+    res += isPart ? +num : 0
+  }
+
+  return res
+}
+
+const part2 = input => {
+  let grid = input.toGrid()
+
+  let res = 0
+  let stars = {}
+  for (let i = 0; i < grid.length; i++) {
+    let row = grid[i]
+
+    let num = ''
+    let starPos
+    for (let j = 0; j < row.length; j++) {
+      let cell = row[j]
+      if (!isNaN(cell)) {
+        num += cell
+
+        grid.forEachSurrounding(i, j, (char, x, y) => {
+          if (char === '*') {
+            starPos = [x, y]
+          }
+        })
+      } else {
+        if (starPos) {
+          stars[starPos] ??= []
+          stars[starPos].push(num)
+          res += +num
+        }
+
+        num = ''
+        starPos = undefined
+      }
+    }
+
+    if (starPos) {
+      stars[starPos] ??= []
+      stars[starPos].push(num)
+      res += +num
+    }
+  }
+
+  return Object.values(stars).reduce(
+    (acc, cur) => acc + (cur.length === 2 ? cur[0] * cur[1] : 0),
+    0
+  )
+}
+
+module.exports = { part1, part2 }
