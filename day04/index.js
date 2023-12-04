260215a15d329447f52b90435dfb0e7fa1e3d5de
Marcus Hansson <machalvan@hotmail.com>
2023-12-04 07:54:58 +0100

    Add day 4

---
+++ day04/index.js
@@ -0,0 +1,34 @@
+require('../utils')()
+
+const part1 = input => {
+  return input
+    .toLines()
+    .map(line => {
+      let [_, winning, my] = line.split(/:|\|/)
+      let winningNums = winning.getNums()
+      let myNums = my.getNums()
+      let wins = winningNums.intersection(myNums).length
+
+      return wins > 1 ? 2 ** (wins - 1) : wins
+    })
+    .sum()
+}
+
+const part2 = input => {
+  let copies = Array(input.toLines().length).fill(1)
+
+  input.toLines().map((line, i) => {
+    let [_, winning, my] = line.split(/:|\|/)
+    let winningNums = winning.getNums()
+    let myNums = my.getNums()
+    let wins = winningNums.intersection(myNums).length
+
+    for (let j = i + 1; j < i + 1 + wins; j++) {
+      copies[j] += copies[i]
+    }
+  })
+
+  return copies.sum()
+}
+
+module.exports = { part1, part2 }
