261032097eefcb8bfa60fa02fa684dbf733526a6
Marcus Hansson <machalvan@hotmail.com>
2023-12-09 07:16:24 +0100

    Add day 9

---
+++ day09/index.js
@@ -0,0 +1,53 @@
+require('../utils')()
+
+const part1 = input => {
+  const extrapolate = nums => {
+    let newNums = nums.reduce((acc, cur, i) => {
+      let newNum = nums[i + 1] - cur
+      if (isNaN(newNum)) return acc
+      acc.push(newNum)
+      return acc
+    }, [])
+
+    if (newNums.count(0) === newNums.length) {
+      return nums.pop()
+    }
+
+    return nums.pop() + extrapolate(newNums)
+  }
+
+  return input
+    .toLines()
+    .map(line => {
+      let nums = line.getNums()
+      return extrapolate(nums)
+    })
+    .sum()
+}
+
+const part2 = input => {
+  const extrapolate = nums => {
+    let newNums = nums.reduce((acc, cur, i) => {
+      let newNum = cur - nums[i + 1]
+      if (isNaN(newNum)) return acc
+      acc.push(newNum)
+      return acc
+    }, [])
+
+    if (newNums.count(0) === newNums.length) {
+      return nums.pop()
+    }
+
+    return nums.pop() - extrapolate(newNums)
+  }
+
+  return input
+    .toLines()
+    .map(line => {
+      let nums = line.getNums().reverse()
+      return extrapolate(nums)
+    })
+    .sum()
+}
+
+module.exports = { part1, part2 }
