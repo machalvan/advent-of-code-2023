c395a9339aafb83f8f78b3f94f90de1eac672930
Marcus Hansson <machalvan@hotmail.com>
2023-12-11 08:04:42 +0100

    Add improved day 11

---
+++ day11/improved.js
@@ -0,0 +1,79 @@
+require('../utils')()
+
+const part1 = input => {
+  let galaxy = input.toGrid()
+  let emptyRows = galaxy
+    .map((_, i) => i)
+    .filter(i => galaxy[i].every(char => char === '.'))
+  let emptyCols = galaxy
+    .map((_, i) => i)
+    .filter(i => galaxy.every(row => row[i] === '.'))
+
+  let pos = []
+  for (let y = 0; y < galaxy.length; y++) {
+    for (let x = 0; x < galaxy[y].length; x++) {
+      if (galaxy[y][x] === '#') {
+        pos.push([x, y])
+      }
+    }
+  }
+
+  let res = 0
+  for (let i = 0; i < pos.length; i++) {
+    for (let j = 0; j < i; j++) {
+      let [x1, y1] = pos[i]
+      let [x2, y2] = pos[j]
+
+      let isBetween = (val, a, b) =>
+        Math.min(a, b) < val && val < Math.max(a, b)
+
+      let extra =
+        emptyRows.map(r => isBetween(r, y1, y2)).sum() +
+        emptyCols.map(c => isBetween(c, x1, x2)).sum()
+
+      res += manhattan(x1, y1, x2, y2) + extra
+    }
+  }
+
+  return res
+}
+
+const part2 = input => {
+  let galaxy = input.toGrid()
+  let emptyRows = galaxy
+    .map((_, i) => i)
+    .filter(i => galaxy[i].every(char => char === '.'))
+  let emptyCols = galaxy
+    .map((_, i) => i)
+    .filter(i => galaxy.every(row => row[i] === '.'))
+
+  let pos = []
+  for (let y = 0; y < galaxy.length; y++) {
+    for (let x = 0; x < galaxy[y].length; x++) {
+      if (galaxy[y][x] === '#') {
+        pos.push([x, y])
+      }
+    }
+  }
+
+  let res = 0
+  for (let i = 0; i < pos.length; i++) {
+    for (let j = 0; j < i; j++) {
+      let [x1, y1] = pos[i]
+      let [x2, y2] = pos[j]
+
+      let isBetween = (val, a, b) =>
+        Math.min(a, b) < val && val < Math.max(a, b)
+
+      let extra =
+        emptyRows.map(r => isBetween(r, y1, y2) * 999_999).sum() +
+        emptyCols.map(c => isBetween(c, x1, x2) * 999_999).sum()
+
+      res += manhattan(x1, y1, x2, y2) + extra
+    }
+  }
+
+  return res
+}
+
+module.exports = { part1, part2 }
