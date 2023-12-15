460f6b50d25a54bb91b3b9fbf932d6ee33b7fec2
Marcus Hansson <machalvan@hotmail.com>
2023-12-15 08:07:05 +0100

    Add improved day 15

---
+++ day15/improved.js
@@ -0,0 +1,47 @@
+require('../utils')()
+
+let hash = str => {
+  return str
+    .split('')
+    .reduce((val, char) => ((val + char.charCodeAt(0)) * 17) % 256, 0)
+}
+
+const part1 = input => {
+  return input.split(',').map(hash).sum()
+}
+
+const part2 = input => {
+  let [lenses, boxes, lensToBox] = input.split(',').reduce(
+    ([lenses, boxes, lensToBox], step) => {
+      let [lens, focal] = step.split(/=|-/)
+      let box = hash(lens)
+
+      if (step.includes('=')) {
+        boxes[box] ??= []
+        if (!boxes[box].includes(lens)) boxes[box].push(lens)
+
+        lensToBox[lens] = box
+        lenses[lens] = +focal
+      } else {
+        boxes[box] = boxes[box]?.filter(boxLens => boxLens !== lens)
+
+        delete lensToBox[lens]
+        delete lenses[lens]
+      }
+
+      return [lenses, boxes, lensToBox]
+    },
+    [{}, {}, {}]
+  )
+
+  return Object.entries(lenses)
+    .map(([lens, focal]) => {
+      let box = lensToBox[lens]
+      let slot = boxes[box].indexOf(lens) + 1
+
+      return (1 + box) * slot * focal
+    })
+    .sum()
+}
+
+module.exports = { part1, part2 }
